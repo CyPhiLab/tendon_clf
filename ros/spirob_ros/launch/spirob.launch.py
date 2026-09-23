d23f4c1d3d0bb2bfd81a8f38e0324400c2486353
Huy Pham <hbp16@case.edu>
2026-09-22 20:55:53 -0400

    Update SpiRob ROS configuration

--- ros/spirob_ros/launch/spirob.launch.py
+++ ros/spirob_ros/launch/spirob.launch.py
@@ -59,8 +59,3 @@ def generate_launch_description():
 
     ])
 
-
-# cd ~/huy/tendon_clf/ros
-# colcon build --symlink-install
-# source install/setup.bash
-# ros2 launch spirob_ros spirob.launch.py
